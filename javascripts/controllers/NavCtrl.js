87504576896112c188333fa376f719fb47ed7e82
ben harrington <harringtonben@icloud.com>
2017-11-17 14:18:41 -0600

    finished authentication

--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,10 +1,10 @@
 'use strict';
 
-app.controller("NavCtrl", function($location, $rootScope, $scope, $window, AuthService){
-    $scope.logoutUser = ()=> {
+app.controller("NavCtrl", function($location, $rootScope, $scope, $window, AuthService) {
+    $scope.logout = () => {
         delete $rootScope.uid;
         $window.localStorage.clear();
         AuthService.logout();
-        $location.path('/login');
+        $location.path('/auth');
     };
 });
