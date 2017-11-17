877e04a79fc346d87614c583249cdeae25e15af6
ben harrington <harringtonben@icloud.com>
2017-11-17 16:19:28 -0600

    finished form and making json package for firsbase post

--- javascripts/controllers/NewCtrl.js
+++ javascripts/controllers/NewCtrl.js
@@ -1,5 +1,20 @@
 'use strict';
 
-app.controller("NewCtrl", function($scope) {
-    $scope.controller = "NewCtrl";
+app.controller("NewCtrl", function($http, $scope, DatabaseService) {
+   $scope.newcontact = {};
+   
+   $scope.submitForm = () => {
+        let newContact = {
+            "first_name": $scope.newcontact.firstname,
+			"last_name": $scope.newcontact.lastname,
+			"phone_number": $scope.newcontact.phonenumber,
+			"email": $scope.newcontact.email,
+			"twitter": $scope.newcontact.twitter,
+			"facebook_page": $scope.newcontact.facebook,
+			"instagram_username": $scope.newcontact.instagram
+        };
+
+        DatabaseService.addNewContact(newContact);
+       
+   };
 });
