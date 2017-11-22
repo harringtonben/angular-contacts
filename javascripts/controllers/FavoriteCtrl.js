c5bc97c8e1f2f7aef3f92f072b80a76d7f31672f
ben harrington <harringtonben@icloud.com>
2017-11-21 18:07:06 -0600

    finished search and some favorites cleanup

--- javascripts/controllers/FavoriteCtrl.js
+++ javascripts/controllers/FavoriteCtrl.js
@@ -1,6 +1,6 @@
 'use strict'; 
 
-app.controller("FavoriteCtrl", function($rootScope, $scope, DatabaseService) {
+app.controller("FavoriteCtrl", function($location, $rootScope, $scope, DatabaseService) {
     const getContacts = () => {
         DatabaseService.getFavoriteContacts($rootScope.uid).then((results) => {
             $scope.contacts = results;
@@ -8,6 +8,22 @@ app.controller("FavoriteCtrl", function($rootScope, $scope, DatabaseService) {
             console.log("error in getContacts");
         });
     };
-    
+
     getContacts();
+
+    $scope.editContact = (contactId) => {
+        $location.path(`/contacts/edit/${contactId}`);
+    };
+
+    $scope.contactDetail = (contactId) => {
+        $location.path(`/contacts/detail/${contactId}`);
+    };
+
+    $scope.deleteContact = (contactId) => {
+        DatabaseService.deleteContact(contactId).then((result) => {
+            getContacts();
+        }).catch((error) => {
+            console.log("error in deleteContact", error);
+        });
+    };
 });
